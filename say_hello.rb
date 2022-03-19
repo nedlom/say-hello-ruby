a94d0e32b8d75f8286ea1ff9e9966ac46961bed3
Nick <nelom3@gmail.com>
2022-03-19 19:38:47 -0400

    commit tree f7a30422062fc488cd6c2d09f82b5b8db36c52c6

--- say_hello.rb
+++ say_hello.rb
@@ -1,1 +1,7 @@
 # Build your say_hello method here
+
+binding.pry
+
+def say_hello(name="Ruby Programmer")
+  puts "Hello #{name}!"
+end
--- spec/say_hello_spec.rb
+++ spec/say_hello_spec.rb
@@ -1,6 +1,8 @@
 require_relative './spec_helper'
 
 describe "say_hello" do 
+
+  binding.pry
   
   it 'accepts an argument of a name and prints out Hello with that Name' do
     expect($stdout).to receive(:puts).with("Hello Kent Beck!")
